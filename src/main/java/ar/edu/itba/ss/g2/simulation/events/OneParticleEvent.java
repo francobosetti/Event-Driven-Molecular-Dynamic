1953ff95eb56041a2b4ced8fd0edc10cd912a0bf
Santiago Ballerini <sballerini@itba.edu.ar>
2024-09-02 17:33:55 -0300

    refactor: rename WallEvent -> OneParticleEvent

--- src/main/java/ar/edu/itba/ss/g2/simulation/events/OneParticleEvent.java
+++ src/main/java/ar/edu/itba/ss/g2/simulation/events/OneParticleEvent.java
@@ -2,11 +2,11 @@ package ar.edu.itba.ss.g2.simulation.events;
 
 import ar.edu.itba.ss.g2.model.Particle;
 
-abstract class WallEvent extends Event {
+abstract class OneParticleEvent extends Event {
     protected final Particle particle;
     protected final int collisionCount;
 
-    public WallEvent(double time, Particle particle) {
+    public OneParticleEvent(double time, Particle particle) {
         super(time);
         this.particle = particle;
         this.collisionCount = particle.getCollisionCount();
--- src/main/java/ar/edu/itba/ss/g2/simulation/events/VerticalWallEvent.java
+++ src/main/java/ar/edu/itba/ss/g2/simulation/events/VerticalWallEvent.java
@@ -2,7 +2,7 @@ package ar.edu.itba.ss.g2.simulation.events;
 
 import ar.edu.itba.ss.g2.model.Particle;
 
-public class VerticalWallEvent extends WallEvent {
+public class VerticalWallEvent extends OneParticleEvent {
     public VerticalWallEvent(double time, Particle particle) {
         super(time, particle);
     }
