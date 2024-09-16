5bf609e947c5a33598b777166f1b2c63fa9d1d9f
Santiago Ballerini <sballerini@itba.edu.ar>
2024-09-15 23:07:28 -0300

    feat: add auxiliary methods to detect collisions

--- analyze/utils.py
+++ analyze/utils.py
@@ -1,5 +1,6 @@
 import math
 
+
 # Load static configuration
 def load_static_data(static_file):
     with open(static_file, "r") as file:
@@ -65,97 +66,129 @@ def load_dynamic_data(dynamic_file):
 
     return times, particle_data
 
+# Returns a dict [time, (id, ((x1, y1, vx1, vy1), (x2, y2, vx2, vy2)))]
+def get_one_particle_collisions(
+    times, particle_data
+):
 
-def get_collision_with_obstacle_count(times, particle_data, obstacle_radius, particle_radius):
-    
-    epsilon = 1e-5
-
-    # Map of time to collision count
-
-    collision_count = {}
-
-    print(f"Collision count: {len(times) - 1}")
-
-    count = 0
+    collision_times = {}
 
     # From 1 to len(times) - 1
     for i in range(1, len(times)):
 
         collisions = []
 
-        for (_, _, vx1, vy1), (x2, y2, vx2, vy2) in zip(particle_data[i - 1], particle_data[i]):
-            
+        for particle_id, ((x1, y1, vx1, vy1), (x2, y2, vx2, vy2)) in enumerate(
+            zip(particle_data[i - 1], particle_data[i])
+        ):
+
             # There is a collision if particle speed changes in the next time step
             if vx1 != vx2 or vy1 != vy2:
-                collisions.append((x2, y2))
+                collisions.append(
+                    (particle_id, ((x1, y1, vx1, vy1), (x2, y2, vx2, vy2)))
+                )
 
         # If there are two collisions in the same time step
         # Skip as it is between two particles
         if len(collisions) != 1:
             continue
 
-        x, y = collisions[0]
+        collision = collisions[0]
 
-        # Check if the collision is with the obstacle
-        center_to_center_distance = math.sqrt(x ** 2 + y ** 2)
-
-        if center_to_center_distance <= obstacle_radius + particle_radius + epsilon:
-            count += 1
-            collision_count[times[i]] = count
+        collision_times[times[i]] = collision
 
 
-    print(f"Obstacle collision count: {count}")
+    return collision_times
 
-    return collision_count
+def get_collisions_with_obstacle(
+    times, particle_data, obstacle_radius, particle_radius
+):
 
-def get_first_collision_with_obstacle_count(times, particle_data, obstacle_radius, particle_radius):
+    epsilon = 1e-5
     
+    collisions = get_one_particle_collisions(times, particle_data)
+
+    collisions_with_obstacle = {}
+
+    for time, (id, ((x1, y1, vx1, vy1), (x2, y2, vx2, vy2))) in collisions.items():
+
+        center_to_center_distance = math.sqrt(x2 ** 2 + y2 ** 2)
+        distance_to_obstacle = abs(center_to_center_distance - (obstacle_radius + particle_radius))
+
+        if distance_to_obstacle <= epsilon:
+            collisions_with_obstacle[time] = (id, ((x1, y1, vx1, vy1), (x2, y2, vx2, vy2)))
+
+    return collisions_with_obstacle
+
+def get_collision_with_wall(times, particle_data, domain_radius, particle_radius):
+
     epsilon = 1e-5
 
-    # Map of time to collision count
+    collisions = get_one_particle_collisions(times, particle_data)
 
-    already_collided_particles = set()
+    wall_collisions = {}
+
+    for time, (id, ((x1, y1, vx1, vy1), (x2, y2, vx2, vy2))) in collisions.items():
+
+        center_to_center_distance = math.sqrt(x2 ** 2 + y2 ** 2)
+        distance_to_wall = abs(domain_radius - (center_to_center_distance + particle_radius))
+
+        if distance_to_wall <= epsilon:
+            wall_collisions[time] = (id, ((x1, y1, vx1, vy1), (x2, y2, vx2, vy2)))
+
+
+def get_collision_with_obstacle_count(
+    times, particle_data, obstacle_radius, particle_radius
+):
+
+    collisions = get_collisions_with_obstacle(
+        times, particle_data, obstacle_radius, particle_radius
+    )
 
     collision_count = {}
-
     count = 0
 
-    # From 1 to len(times) - 1
-    for i in range(1, len(times)):
-
-        collisions = []
-
-        for particle_id, ((_, _, vx1, vy1), (x2, y2, vx2, vy2)) in enumerate(zip(particle_data[i - 1], particle_data[i])):
-            
-            # There is a collision if particle speed changes in the next time step
-            if vx1 != vx2 or vy1 != vy2:
-                collisions.append((particle_id, (x2, y2)))
-
-        # If there are two collisions in the same time step
-        # Skip as it is between two particles
-        if len(collisions) != 1:
-            continue
-
-        particle_id, (x, y) = collisions[0]
-
-        if particle_id in already_collided_particles:
-            continue
-
-
-        # Check if the collision is with the obstacle
-        if math.sqrt(x ** 2 + y ** 2) <= obstacle_radius + particle_radius + epsilon:
-            already_collided_particles.add(particle_id)
-            count += 1
-            collision_count[times[i]] = count
+    for time, _ in collisions.items():
+        count += 1
+        collision_count[time] = count
 
     return collision_count
 
-        
+
+def get_first_collision_with_obstacle_count(
+    times, particle_data, obstacle_radius, particle_radius
+):
+
+    collisions = get_collisions_with_obstacle(
+        times, particle_data, obstacle_radius, particle_radius
+    )
+
+    collision_count = {}
+    already_collided = set()
+    count = 0
+
+    for time, (id, _) in collisions.items():
+        if id in already_collided:
+            continue
+
+        count += 1
+        collision_count[time] = count
+        already_collided.add(id)
+
+    return collision_count
 
 
-
-            
-            
+def get_system_temperature(particle_data, particle_mass):
 
 
-            
+    # All collisions are elastic, so the energy is conserved
+    velocities = particle_data[0]
+    particle_count = len(velocities)
+
+    kinetic_energy = 0
+
+    for _, _, vx, vy in velocities:
+        kinetic_energy += 0.5 * particle_mass * (vx ** 2 + vy ** 2)
+
+    return kinetic_energy / particle_count
+
